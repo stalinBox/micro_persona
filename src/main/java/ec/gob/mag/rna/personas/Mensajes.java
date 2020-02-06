cd6590437070dc2a88e60d66ff9fb0e12740bf5c
Juan Carlos Guevara Morocho <jguevaram@mag.gob.ec>
2020-02-06 16:02:01 -0500

    mensajes

--- src/main/java/ec/gob/mag/rna/personas/Mensajes.java
+++ src/main/java/ec/gob/mag/rna/personas/Mensajes.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class Mensajes {
-
+	
 	@Bean
 	public ResourceBundleMessageSource messageSource() {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("messages");
-		return messageSource;
+           ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+           messageSource.setBasename("messages_es");
+           return messageSource;
 	}
+
 }
